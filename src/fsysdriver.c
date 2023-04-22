908eb0ce89156c54b35825c4ba432f80aa1470fe
Caleb Powell <powellcalebm@gmail.com>
2023-04-22 17:11:13 -0500

    uncomment un-needed code

--- src/fsysdriver.c
+++ src/fsysdriver.c
@@ -21,17 +21,18 @@ void printBuffer(uint8_t buffer[],int size) {
 
 
 int main(int argc, char * argv[]) { 
-  uint8_t buffer1[BLOCK_SIZE],buffer2[BLOCK_SIZE]; 
-  int i; 
+  // uint8_t buffer1[BLOCK_SIZE],buffer2[BLOCK_SIZE]; // I have no idea why this is here
+  // int i; // I have no idea why this is here
   readImage("image1.img"); 
-  // makeFreeList(); 
+  makeFreeList(); 
   cpmDir(); 
-  // printFreeList(); 
-  // cpmDelete("shortf.ps");
-  // cpmDir();
-  // cpmRename("mytestf1.txt","mytest2.tx");
-  // fprintf(stdout,"cpmRename return code = %d,\n",cpmRename("mytestf","mytestv2.x")); 
-  // cpmDir(); 
-  // printFreeList(); 
+  printFreeList(); 
+  cpmDelete("shortf.ps");
+  cpmDir();
+  cpmRename("mytestf1.txt","mytest2.tx");
+  fprintf(stdout,"cpmRename return code = %d,\n",cpmRename("mytestf","mytestv2.x"));
+  cpmDir(); 
+  printFreeList();
+  return 0; // added bc non-void function
 }
 
