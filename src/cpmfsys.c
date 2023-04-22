2a2ba8df0c1e47dda25f767e208d110204f20d22
Caleb Powell <powellcalebm@gmail.com>
2023-04-21 21:11:09 -0500

    mkDirStruct() rewrite with memcpy()

--- src/cpmfsys.c
+++ src/cpmfsys.c
@@ -12,64 +12,34 @@ DirStructType *mkDirStruct(int index, uint8_t *e)
     // create a new pointer to a DirStructType named "d" and allocate memory
     DirStructType *d = (DirStructType *)malloc(sizeof(DirStructType));
 
-    // copy status
-    d->status = dir_addr[0]; // status is only 1 byte
-
-    // copy name + null terminator
-    int name_index; // index for the name
-    char name_letter; // temp storage for the letter at that index
-    for(name_index = 0; name_index<8; name_index++){
-        name_letter = dir_addr[name_index+1]; // store the letter found
-        // check if the char is a space character (32 in ASCII)
-        if( name_letter != 32){
-            // printf("%c\n",letter); // print the letter
-            d->name[name_index] = name_letter; // store the letter
-        }
-        else{
+    // find name length
+    int name_length;
+    for(name_length = 0; name_length<8; name_length++){
+        // if its a space char
+        if( dir_addr[name_length+1] == 32){
             break;
         }
     }
-    d->name[name_index+1] = '\0'; // add null terminator
-    // printf("%s\n", d->name); // print the stored name
-
-    // copy extension + null terminator
-    int ext_index; // index for ext
-    char ext_letter; // temp storage for the letter at that index
-    for(ext_index=0; ext_index<3; ext_index++){
-        ext_letter = dir_addr[ext_index+9]; // store the letter found
-        // check if the char is a space character (32 in ASCII)
-        if( ext_letter != 32){
-            // printf("%c\n", letter); // print the letter
-            d->extension[ext_index] = ext_letter;
-        }
-        else{
+    
+    
+    // find extension length
+    int extension_length; // index for ext
+    for(extension_length=0; extension_length<3; extension_length++){
+        if(dir_addr[extension_length+9] == 32){
             break;
         }
     }
-    d->extension[ext_index+1] = '\0'; // add null terminator
-    // printf("%s\n", d->extension); // print the stored name
-
-    // copy BC
-    d->BC = dir_addr[13]; // 13 bc that's where the BC is stored in extent
-
-    // copy RC
-    d->RC = dir_addr[15]; // +2 bc RC is 2 bytes
-
-    // for loop to copy block addresses
-    int block_index; // index for the block
-    uint8_t block; // place to store block value
-    for (block_index = 0; block_index < 16; block_index++){
-        block = dir_addr[block_index+16]; // store the block
-        // check if the block is valid
-        if (block != 0){
-            // printf("%d\n", block); // print the block value
-            d->blocks[block_index] = block; // copy block data
-        }
-        else{
-            break;
-        }
-    }
-
+    
+    // copy memory
+    memcpy(&d->name, dir_addr + 1, name_length); // copy name
+    d->name[name_length+1] = '\0'; // null terminate
+    memcpy(&d->extension, dir_addr + 9, extension_length); // copy extension
+    d->extension[extension_length+1] = '\0'; // null terminate
+    memcpy(&d->status, dir_addr, 1); // copy status   
+    memcpy(&d->BC, dir_addr + 13, 1); // copy BC
+    memcpy(&d->RC, dir_addr + 15, 1); // copy RC
+    memcpy(&d->blocks, dir_addr + 16, 16); // copy blocks
+    
     return d;
 }
 
